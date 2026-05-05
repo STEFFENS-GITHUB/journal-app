3f0ba6922703454996bb5b5d0a8f0e01fccbd698
Steffen <Redacted>
2026-05-04 21:34:31 -0500

    Updated database url logic

--- app/database/session.py
+++ app/database/session.py
@@ -1,14 +1,26 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.pool import NullPool
-import os
+import os, json
 
-if os.getenv("DOCKER_ENV") == "1":
+if os.getenv("DATABASE_URL"):
     db_url = os.getenv("DATABASE_URL")
 else:
-    db_url="mysql+asyncmy://root:123@localhost:3306/test"
+    secret = os.getenv("DB_MASTER_SECRET")
+    if not secret:
+        raise RuntimeError("Missing DB configuration. (Missing required env vars)")
 
-if not db_url:
-    raise RuntimeError("db_url is not set")
+    secret_json = json.loads(secret)
+    username = secret_json["username"]
+    password = secret_json["password"]
+    host = os.getenv("DB_ENDPOINT")
+    dbname = os.getenv("DB_NAME")
+    port = os.getenv("DB_PORT", "3306")
+
+    if not host or not dbname:
+        raise RuntimeError("Missing DB env vars")
+    
+    db_url = f"mysql+asyncmy://{username}:{password}@{host}:{port}/{dbname}"
+
 engine = create_async_engine(db_url, echo=True, poolclass=NullPool)
 AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
